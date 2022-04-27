03575b13097c6716458b183606bce11d4ee198e3
Andrei Dashkevich <andrei-dashkevich@mail.ru>
2022-04-27 20:02:40 +0300

    lesson automatisation cinema questions

--- js/script1.js
+++ js/script1.js
@@ -15,3 +15,59 @@
 'use strict';
 
 // Код возьмите из предыдущего домашнего задания
+
+const minimumOfSymbols = 3;
+const VIEWER_STATUS = {
+    no_Status:{count: {begin: 0, end: 9}, statusName: "без статуса"},
+    classic: {count: {begin: 10, end: 30}, statusName: "классический зритель"},
+    kinoman: {count: {begin: 31, end: "-"}, statusName: "киноман"}
+};
+
+
+let personalMovieDB = {
+    count: '0',
+    movies: [],
+    actors: {},
+    genres: {},
+    privat: false
+}
+
+
+
+while (1) {
+    let answer = prompt('Сколько фильмов вы уже посмотрели?', "0");
+    if(answer.length == 0 ){
+        alert("Введите число");
+        continue;
+    } else {
+        personalMovieDB['count'] = answer;
+        break;
+    }
+
+}
+
+while (1) {
+    let answer = prompt('Один из последних просмотренных фильмов?', "");
+    if(answer.length == 0 ){
+        alert("Введите текст");
+        continue;
+    } else if(answer.length < minimumOfSymbols){
+        alert("Текст должен содержать не меньше 3х символов");
+        continue;
+    } else {
+        personalMovieDB.movies.push({"name": answer});
+        break;
+    }
+        
+}
+
+if( personalMovieDB['count'] <= VIEWER_STATUS.no_Status.count.end)
+    alert("Просмотрено довольно мало фильмов");
+else if (personalMovieDB['count'] <= VIEWER_STATUS.classic.count.end) {
+    alert(`Вы ` + VIEWER_STATUS.classic.statusName);
+} else if (personalMovieDB['count'] >= VIEWER_STATUS.kinoman.count.begin) {
+    alert(`Вы ` + VIEWER_STATUS.kinoman.statusName);
+}
+
+
+console.log(personalMovieDB); 
