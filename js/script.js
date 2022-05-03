4e5afe8689ba6275647161d387c547bf9a6fe1df
Andrei Dashkevich <andrei-dashkevich@mail.ru>
2022-05-03 16:21:12 +0300

    functions with * 2

--- js/script.js
+++ js/script.js
@@ -1,32 +1,60 @@
 'use district';
 
-// Место для первой задачи
-function calculateVolumeAndArea(side) {
-    let result = "";
-    if(!Number.isInteger(side) || typeof(side) == typeof("string") || side < 0 )
-        result = "При вычислении произошла ошибка";
-    else
-        result = `Объем куба: ${side * side * side}, площадь всей поверхности: ${side * side * 6}`;
-    return result;
-}
+console.log(getTimeFromMinutes(150));
+console.log(getTimeFromMinutes(84));
+console.log(getTimeFromMinutes(-50));
+console.log(getTimeFromMinutes(0));
+console.log(getTimeFromMinutes('150'));
 
-// Место для второй задачи
-function getCoupeNumber(number) {
+console.log("_____");
+
+console.log(findMaxNumber(1, 5, 6.6, 11));
+console.log(findMaxNumber(1, 5, '6', '10'));
+
+
+
+
+// Место для первой задачи
+function getTimeFromMinutes(inputMinutes) {
     let result = "";
-    if(!Number.isInteger(number) || typeof(number) == typeof("string") || number < 0)
-        result = "Ошибка. Проверьте правильность введенного номера места";
-    else if (number = 0 || number > 36) {
-        result = "Таких мест в вагоне не существует";
+    let hours = 0,
+        minutes = 0;
+
+    function setCorrectName(number, name){
+        let result = "";
+        let lastDigit = ("" + number);
+        lastDigit = (number > 20) ? lastDigit[lastDigit.length-1]: lastDigit;
+
+        if (lastDigit == 1) {
+            result = (name == "m") ? "минута" : (name == "h") ? "час" : name;
+        } else if(lastDigit >= 2 && lastDigit <= 4){
+            result = (name == "m") ? "минуты" : (name == "h") ? "часа" : name;
+        } else if (lastDigit >= 5 || lastDigit == 0){
+            result = (name == "m") ? "минут" : (name == "h") ? "часов" : name;
+        }
+        return result;
+    }
+
+    if(inputMinutes % 1 !== 0 || inputMinutes < 0 || typeof(inputMinutes) === typeof("str")){
+        result = "Ошибка, проверьте данные";
     } else{
-        result = Math.ceil((number / 4));
+        hours = Math.floor(inputMinutes / 60);
+        minutes = inputMinutes % 60;
+        result = `Это ${hours} ${setCorrectName(hours, 'h')} и ${minutes} ${setCorrectName(minutes, 'm')}`;
     }
     return result;
 }
 
-console.log(calculateVolumeAndArea(5));
-console.log(calculateVolumeAndArea(15));
-console.log(calculateVolumeAndArea(15.5));
-console.log(calculateVolumeAndArea('15'));
-console.log(calculateVolumeAndArea(-15));
-
-console.log(calculateVolumeAndArea(5));
+// Место для второй задачи
+function findMaxNumber(num1, num2, num3, num4) {
+    let result = 0;
+    if( typeof(num1) !== 'number' || 
+        typeof(num2) !== 'number' || 
+        typeof(num3) !== 'number' || 
+        typeof(num4) !== 'number')
+        {result = 0;}
+    else {
+        result = Math.max(num1, num2 , num3, num4);
+    }
+    return result;
+}
