354f98672768ee4a6887c75c92af72034f6ac808
Andrei Dashkevich <andrei-dashkevich@mail.ru>
2022-05-05 16:25:10 +0300

    fibonachi

--- js/script.js
+++ js/script.js
@@ -1,60 +1,27 @@
 'use district';
 
-console.log(getTimeFromMinutes(150));
-console.log(getTimeFromMinutes(84));
-console.log(getTimeFromMinutes(-50));
-console.log(getTimeFromMinutes(0));
-console.log(getTimeFromMinutes('150'));
+console.log(fib(4));
+console.log(fib(7));
+console.log(fib('7'));
+console.log(fib(""));
+console.log(fib(0));
 
-console.log("_____");
-
-console.log(findMaxNumber(1, 5, 6.6, 11));
-console.log(findMaxNumber(1, 5, '6', '10'));
-
-
-
-
-// Место для первой задачи
-function getTimeFromMinutes(inputMinutes) {
-    let result = "";
-    let hours = 0,
-        minutes = 0;
-
-    function setCorrectName(number, name){
-        let result = "";
-        let lastDigit = ("" + number);
-        lastDigit = (number > 20) ? lastDigit[lastDigit.length-1]: lastDigit;
-
-        if (lastDigit == 1) {
-            result = (name == "m") ? "минута" : (name == "h") ? "час" : name;
-        } else if(lastDigit >= 2 && lastDigit <= 4){
-            result = (name == "m") ? "минуты" : (name == "h") ? "часа" : name;
-        } else if (lastDigit >= 5 || lastDigit == 0){
-            result = (name == "m") ? "минут" : (name == "h") ? "часов" : name;
+function fib(numOfDigits) {
+    let result = "0 1";
+    const arr = [+result[0], +result[2]];
+    
+    if (numOfDigits <= 0 || typeof(numOfDigits) != "number" || numOfDigits % 1 !== 0) {
+        result = "";
+    }else if (numOfDigits == 1) {
+        result = "0";
+    }else if (numOfDigits == 2) {
+        result = "0 1";
+    }else {
+        for (let i = 2; i < numOfDigits; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
+            result += ` ${arr[i]}`;
         }
-        return result;
     }
 
-    if(inputMinutes % 1 !== 0 || inputMinutes < 0 || typeof(inputMinutes) === typeof("str")){
-        result = "Ошибка, проверьте данные";
-    } else{
-        hours = Math.floor(inputMinutes / 60);
-        minutes = inputMinutes % 60;
-        result = `Это ${hours} ${setCorrectName(hours, 'h')} и ${minutes} ${setCorrectName(minutes, 'm')}`;
-    }
-    return result;
-}
-
-// Место для второй задачи
-function findMaxNumber(num1, num2, num3, num4) {
-    let result = 0;
-    if( typeof(num1) !== 'number' || 
-        typeof(num2) !== 'number' || 
-        typeof(num3) !== 'number' || 
-        typeof(num4) !== 'number')
-        {result = 0;}
-    else {
-        result = Math.max(num1, num2 , num3, num4);
-    }
     return result;
 }
